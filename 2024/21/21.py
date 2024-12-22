c7945186693b78a6912bd088c4bf9ff8999c4a20
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2024-12-22 11:53:50 +0000

    2024 21 part 2

--- 2024/21/21.py
+++ 2024/21/21.py
@@ -1,34 +1,16 @@
-grid = [["7","8","9"],["4","5","6"],["1","2","3"],["_","0","A"]]
-coords = {c: (i,j) for i,l in enumerate(grid) for j,c in enumerate(l)}
+grid_num = [["7","8","9"],["4","5","6"],["1","2","3"],["_","0","A"]]
+coords_num = {c: (i,j) for i,l in enumerate(grid_num) for j,c in enumerate(l)}
+
 def paths_between_nums(num1, num2):
-    c1 = coords[num1]
-    c2 = coords[num2]
-    vertical = ""
-    if c1[0]>c2[0]:
-        vertical = "^"*(c1[0]-c2[0])
-    elif c1[0]<c2[0]:
-        vertical = "v"*(c2[0]-c1[0])
-    horizontal = ""
-    if c1[1]>c2[1]:
-        horizontal = "<"*(c1[1]-c2[1])
-    elif c1[1]<c2[1]:
-        horizontal = ">"*(c2[1]-c1[1])
+    c1 = coords_num[num1]
+    c2 = coords_num[num2]
+    vertical = "^"*(c1[0]-c2[0])+"v"*(c2[0]-c1[0])
+    horizontal = "<"*(c1[1]-c2[1])+">"*(c2[1]-c1[1])
     if c1[0]==3 and c2[1]==0:
         return [vertical+horizontal]
     if c1[1]==0 and c2[0]==3:
         return [horizontal+vertical]
     return list(set([vertical+horizontal, horizontal+vertical]))
-    
-def num_to_dir(s):
-    possible_out = [p+"A" for p in paths_between_nums("A", s[0])]
-    for i in range(len(s)-1):
-        n = []
-        p = paths_between_nums(s[i], s[i+1])
-        for prev in possible_out:
-            for path in p:
-                n.append(prev+path+"A")
-        possible_out = n
-    return possible_out
 
 grid_dir = [["_","^","A"],["<","v",">"]]
 coords_dir = {c: (i,j) for i,l in enumerate(grid_dir) for j,c in enumerate(l)}
@@ -36,41 +18,40 @@ coords_dir = {c: (i,j) for i,l in enumerate(grid_dir) for j,c in enumerate(l)}
 def paths_between_dirs(dir1, dir2):
     c1 = coords_dir[dir1]
     c2 = coords_dir[dir2]
-    vertical = ""
-    if c1[0]>c2[0]:
-        vertical = "^"*(c1[0]-c2[0])
-    elif c1[0]<c2[0]:
-        vertical = "v"*(c2[0]-c1[0])
-    horizontal = ""
-    if c1[1]>c2[1]:
-        horizontal = "<"*(c1[1]-c2[1])
-    elif c1[1]<c2[1]:
-        horizontal = ">"*(c2[1]-c1[1])
+    vertical = "^"*(c1[0]-c2[0])+"v"*(c2[0]-c1[0])
+    horizontal = "<"*(c1[1]-c2[1])+">"*(c2[1]-c1[1])
     if c1[0]==0 and c2[1]==0:
         return [vertical+horizontal]
     if c1[1]==0 and c2[0]==0:
         return [horizontal+vertical]
     return list(set([vertical+horizontal, horizontal+vertical]))
 
-def dir_to_dir(s):
-    possible_out = [p+"A" for p in paths_between_dirs("A", s[0])]
-    for i in range(len(s)-1):
-        n = []
-        p = paths_between_dirs(s[i], s[i+1])
-        for prev in possible_out:
-            for path in p:
-                n.append(prev+path+"A")
-        possible_out = n
-    return possible_out
+from functools import cache
+@cache
+def num_moves(start, end, layer, top_layer):
+    grid = grid_num if layer == 0 else grid_dir
+    coords = coords_num if layer == 0 else coords_dir
+    if start == end:
+        return 1
+    c1 = coords[start]
+    c2 = coords[end]
+    if layer == top_layer-1:
+        return abs(c1[0]-c2[0])+abs(c1[1]-c2[1])+1
+    candidates = []
+    possibilities = paths_between_nums(start, end) if layer == 0 else paths_between_dirs(start, end)
+    for pos in possibilities:
+        steps = num_moves("A", pos[0], layer+1, top_layer)
+        for i in range(len(pos)-1):
+            steps += num_moves(pos[i], pos[i+1], layer+1, top_layer)
+        steps += num_moves(pos[-1], "A", layer+1, top_layer)
+        candidates.append(steps)
+    return min(candidates)
 
-out = 0
-for line in open("input.txt").read().splitlines():
-    dir1 = num_to_dir(line)
-    dir2 = []
-    for d in dir1:
-        dir2.extend(dir_to_dir(d))
-    dir3 = []
-    for d in dir2:
-        dir3.extend(dir_to_dir(d))
-    out += int(line[:3]) * min(len(d) for d in dir3)
-print(out)
+for depth in [2, 25]:
+    out = 0
+    for line in open("input.txt").read().splitlines():
+        complexity = num_moves("A", line[0], 0, depth+1)
+        for i in range(len(line)-1):
+            complexity += num_moves(line[i], line[i+1], 0, depth+1)
+        out += complexity * int(line[:-1])
+    print(out)
