af3a5cc6a38754cd4bde42d31e44f6ecbe1cd80f
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2025-12-02 13:18:20 +0000

    2025 02 one liners

--- 2025/02/02.py
+++ 2025/02/02.py
@@ -13,3 +13,8 @@ for pair in open("input.txt").read().strip().split(","):
                 invalids.add(id)
 print(out)
 print(sum(invalids))
+
+print()
+
+print(sum(sum(n for n in range(a,b+1) if (s:=str(n))[:len(s)//2]==s[len(s)//2:]) for a,b in (map(int, pair.split("-")) for pair in open("input.txt").read().strip().split(","))))
+print(sum(set(i for a,b in (map(int, pair.split("-")) for pair in open("input.txt").read().strip().split(",")) for r in range(1,len(str(b))) for i in range(a,b+1) if (r_:=len(str(i))//r) > 1 and str(i)=="".join(str(i)[:r] for _ in range(r_)))))
